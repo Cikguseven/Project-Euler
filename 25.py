fa57b015b366d2cf202c5f5dc537089be878caa8
Kieron Seven Lee <kieronsevenlee@gmail.com>
2020-05-14 19:00:31 +0800

    Update 25.py

--- 25.py
+++ 25.py
@@ -23,15 +23,28 @@ The 12th term, F12, is the first term to contain three digits.
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 '''
 
+import time
+
+start = time.time() # Time at start of execution
+
 a = 1
 b = 1
 n = 2
+x = 10 ** 999 # Smallest 1000 digit number
 
-while len(str(a + b)) < 1000: # Generate Fibonacci numbers until number reaches length of 1000 and iterate index
-	a = a + b
-	n += 1
-	b = a + b
-	n += 1
+while True: # Generate Fibonacci numbers and stops if number reaches length of 1000
+    a = a + b
+    n += 1
+    if a >= x:
+        break
+    else:
+        b = a + b
+        n += 1
+        if b >= x:
+            break
 
-print(n) # Index of first term with 1000 digits
+print(n) # Prints index of first term with 1000 digits
 
+end = time.time() # Time at end of execution
+
+print(end - start) # Total execution time in seconds
