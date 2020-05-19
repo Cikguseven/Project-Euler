c83dca001c943a3c2ff4c12c4ab3cb3c1545909b
Kieron Seven Lee <kieronsevenlee@gmail.com>
2020-05-19 21:11:58 +0800

    Updated 22.py

--- Solutions/22.py
+++ Solutions/22.py
@@ -5,9 +5,14 @@ For example, when the list is sorted into alphabetical order, COLIN, which is wo
 
 What is the total of all the name scores in the file?
 '''
-nameFile = open('22_names.txt', 'r')
+import os
+
+cur_path = os.path.dirname(__file__)
+
+new_path = os.path.relpath('..\\Additional files\\22_names.txt', cur_path)
+
+nameFile = open(new_path, 'r')
 nameList = sorted(nameFile.read().replace('"','').split(','),key=str)
-total = 0
 
 letterToNumDict = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
 
@@ -17,10 +22,12 @@ def letterToNum(nstring): # Converts letters in name to equivalent numerical val
 		x += letterToNumDict[i]
 	return x
 
+total = 0
 
 for name in nameList: # Sums product of numerical value of name and rank in alphabetical order
 	total += letterToNum(name) * (nameList.index(name) + 1)
 	
 print(total)
 
+nameFile.close()
 
